8c42c6bd9422e78cbfcf1d41587975763d523471
Alex Wayne <alex.wayne.dev@gmail.com>
2023-09-14 03:14:40 -0700

    Works for basic arithmetic +-*/

--- main.go
+++ main.go
@@ -14,9 +14,16 @@ const (
 )
 
 type Token struct {
-	kind  int
-	value string
+	kind      int
+	value     string
 }
+
+type Component struct {
+	operands [2]int
+	operator rune
+}
+
+type Expression []Token
 
 func collectCurrentToken(currentString []rune) (Token, error) {
 	s := string(currentString)
@@ -30,7 +37,6 @@ func collectCurrentToken(currentString []rune) (Token, error) {
 func lex(input string) ([]Token, error) {
 
 	var tokens []Token
-
 	currentString := []rune{}
 
 	for i := 0; i < len(input); i++ {
@@ -61,7 +67,7 @@ func lex(input string) ([]Token, error) {
 	}
 
 	if len(currentString) > 0 {
-		token, err := collectCurrentToken(currentString[0:len(currentString) - 1])
+		token, err := collectCurrentToken(currentString[0 : len(currentString)-1])
 		if err != nil {
 			return []Token{}, err
 		}
@@ -69,7 +75,115 @@ func lex(input string) ([]Token, error) {
 	}
 
 	return tokens, nil
+}
 
+func collectComponents(tokens []Token) ([]Token, error) {
+	rem_tokens := []Token{}
+
+	for i := 0; i < len(tokens); i++ {
+		remaining := len(tokens) - i
+
+		if remaining < 0 { 
+			break
+		}
+
+		token := tokens[i]
+
+		if token.kind == OPERATOR {
+			operator := rune(token.value[0])
+			if i == 0 {
+				return nil, errors.New("Invalid expression - operator at beginning of expression")
+			}
+			if remaining == 0 {
+				return nil, errors.New("Invalid expression - operator at end of expression")
+			}
+
+			if tokens[i-1].kind == OPERATOR || tokens[i+1].kind == OPERATOR {
+				return nil, errors.New("Invalid expression - two operators in a row")
+			}
+
+			prevNum, err := strconv.Atoi(tokens[i-1].value)
+			if err != nil {
+				return nil, errors.New("Token before operator is not a number")
+			}
+
+			nextNum, err := strconv.Atoi(tokens[i+1].value)
+			if err != nil {
+				return nil, errors.New("Token after operator is not a number")
+			}
+
+			if operator == '*' || operator == '/' {
+				comp := Component{}
+				comp.operands[0] = prevNum
+				comp.operands[1] = nextNum
+				comp.operator = operator
+
+				rem_tokens = rem_tokens[0 : len(rem_tokens)-1]
+
+				if operator == '*' {
+					rem_tokens = append(rem_tokens, Token{NUMBER, strconv.Itoa(prevNum * nextNum)})
+				} else {
+					rem_tokens = append(rem_tokens, Token{NUMBER, strconv.Itoa(prevNum / nextNum)})
+				}
+				i++
+
+			} else {
+				rem_tokens = append(rem_tokens, token)
+			}
+		} else {
+			rem_tokens = append(rem_tokens, token)
+		}
+	}
+
+	return rem_tokens, nil
+}
+
+func parse(tokens []Token) (Expression, error) {
+	expression := Expression{}
+
+	expression, err := collectComponents(tokens)
+	if err != nil {
+		return expression, err
+	}
+
+	return expression, nil
+}
+
+func evaluate(expression Expression) (int, error) {
+
+	running_total := -1
+
+	for i, token := range expression {
+		if token.kind == OPERATOR {
+			operator := rune(token.value[0])
+
+			prev_num, err := strconv.Atoi(expression[i-1].value)
+			if err != nil {
+				return 0, err
+			}
+
+			next_num, err := strconv.Atoi(expression[i+1].value)
+			if err != nil {
+				return 0, err
+			}
+		
+			if operator == '+' {
+				if running_total == -1 {
+					running_total = prev_num + next_num
+				} else {
+					running_total += next_num
+				}
+			} else if operator == '-' {
+				if running_total == -1 {
+					running_total = prev_num - next_num
+				} else {
+					running_total -= next_num
+				}
+			}
+		}
+	}
+
+	return running_total, nil
 }
 
 func main() {
@@ -83,7 +197,17 @@ func main() {
 		return
 	}
 
-	for _, token := range tokens {
-		fmt.Println(token.value)
+	expression, err := parse(tokens)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return
 	}
+
+	result, err := evaluate(expression)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return
+	}
+
+	fmt.Printf("Result: %d\n", result)
 }
